53ef96651bffad070496c9c3ebe09770bbc53274
thuan244 <hoangthuan.1@gmail.com>
2025-06-08 14:49:50 +0700

    update useragent

--- app/api/fetch/route.ts
+++ app/api/fetch/route.ts
@@ -18,7 +18,11 @@ export async function POST(request: Request) {
 
     const res = await fetch(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Referer': 'https://www.google.com/'
       },
       signal: AbortSignal.timeout(10000), // Timeout sau 10 giây
     });
